9d3d53b9303f7e3a2052ee528213574e2ae6e858
dawit95 <dawit0310@gmail.com>
2021-05-21 16:47:34 +0900

    UPDATE store

--- vue-light-bootstrap-dashboard-master/src/store/index.js
+++ vue-light-bootstrap-dashboard-master/src/store/index.js
@@ -7,29 +7,14 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     address: "",
-    sidoList: [],
     sido: "",
-    gugunList: [],
     gugun: "",
-    dongList: [],
     dong: "",
     apts: [],
     apt: {}
   },
   getters: {},
   mutations: {
-    GET_SIDO_LIST(state, sidoList) {
-      // console.log(state, apts);
-      state.sidoList = sidoList;
-    },
-    GET_GUGUN_LIST(state, gugunList) {
-      // console.log(state, apts);
-      state.gugunList = gugunList;
-    },
-    GET_DONG_LIST(state, dongList) {
-      // console.log(state, apts);
-      state.dongList = dongList;
-    },
     GET_APT_LIST(state, apts) {
       state.apts = apts;
     },
@@ -47,45 +32,13 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    getSidoList({ commit }) {
-      axios
-        .get("/address/sido")
-        .then(response => {
-          console.log(response);
-          commit("GET_SIDO_LIST", response);
-        })
-        .catch(error => {
-          alert(error);
-        });
-    },
     // selectSido({ commit }, sido) {
     //   commit('SELECT_SIDO', sido);
     // },
-    getGugunList({ commit }, sido) {
-      commit("SELECT_SIDO", sido);
-      axios
-        .get("/address/gugun?sido=" + sido)
-        .then(response => {
-          commit("GET_GUGUN_LIST", response);
-        })
-        .catch(error => {
-          alert(error);
-        });
-    },
+
     // selectGugun({ commit }, gugun) {
     //   commit('SELECT_GUGUN', gugun);
     // },
-    getDongList({ commit }, gugun) {
-      commit("SELECT_GUGUN", gugun);
-      axios
-        .get("/address/dong?gugun=" + gugun)
-        .then(response => {
-          commit("GET_DONG_LIST", response);
-        })
-        .catch(error => {
-          alert(error);
-        });
-    },
     // selectDong({ commit }, dong) {
     //   commit('SELECT_DONG', dong);
     // },
